c7fd3effd07dbee0e8eb54edfbc11d1bb2a4865c
chekurihemanth <96361549+chekurihemanth@users.noreply.github.com>
2023-11-24 18:46:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function volume_sphere() {
     //Write your code here
-	let r = document.getElementById("radius").value;
-  	let v = document.getElementById("volume");
-    //let num1 = (4/3) * Math.PI*Math.pow(r,3);
-	//let num2 - num1.toFixed(4);
-    //v.value = num2;
-	//return false;
+	let r = document.getElementById('radius').value;
+  	let v = document.getElementById('volume');
+    let num1 = (4/3) * Math.PI*Math.pow(r,3);
+	let num2 - num1.toFixed(4);
+    v.value = num2;
+	return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
